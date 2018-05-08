ae15d49d002a47b993c710160a63ce3f5106ea92
Tor Kjetil Nilsen <tor@arkitektum.no>
2018-05-08 15:11:00 +0200

    more samples

---
+++ Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/GenerateN1.cs
@@ -0,0 +1,32 @@
+﻿using no.geointegrasjon.rep.matrikkelfoering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geointegrasjon.Matrikkelfoering.Sample
+{
+    class GenerateN1
+    {
+        public ByggesakType GenerateSample()
+        {
+            var byggesak = new ByggesakType();
+            byggesak.adresse = "Byggestedgate 1";
+            byggesak.tittel = "Rammesøknad for enebolig i Byggestedgate 1";
+            byggesak.saksnummer = new SaksnummerType() { saksaar = "2018", sakssekvensnummer = "123456" };
+            byggesak.kategori = new ProsesskategoriType() { kode = "RS", beskrivelse = "Søknad om rammetillatelse" };
+            byggesak.tiltakstype = new[] { new TiltaktypeType() { kode = "nyttbyggboligformal", beskrivelse = "Nytt bygg - boligformål" } };
+            byggesak.vedtak = new VedtakType() { beskrivelse = "Vedtak om rammetillatelse", status = new VedtakstypeType() { kode = "1", beskrivelse = "Godkjent" }, vedtaksdato = DateTime.Now };
+
+            byggesak.matrikkelopplysninger = new MatrikkelopplysningerType();
+            byggesak.matrikkelopplysninger.adresse = new[] {
+                new AdresseType(){ adressekode = "1001", adressenavn = "Byggestedgate", adressenummer = "1" } };
+            byggesak.matrikkelopplysninger.eiendomsidentifikasjon = new[] {
+                new MatrikkelnummerType() { kommunenummer = "9999", gaardsnummer = "1", bruksnummer = "2" } };
+            byggesak.matrikkelopplysninger.bygning = new[] { new BygningType() { bebygdAreal = 110.5, bebygdArealSpecified = true, bygningstype = new BygningstypeType() { kode = "111", beskrivelse = "Enebolig" } } };
+
+            return byggesak;
+        }
+    }
+}
--- Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/Program.cs
+++ Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/Program.cs
@@ -20,31 +20,15 @@ namespace Geointegrasjon.Matrikkelfoering.Sample
 
 
             //Datamodell matrikkelføring
-            var byggesak = new ByggesakType();
-            byggesak.adresse = "Byggestedgate 1";
-            byggesak.tittel = "Rammesøknad for enebolig i Byggestedgate 1";
-            byggesak.saksnummer = new SaksnummerType() { saksaar = "2018", sakssekvensnummer = "123456" };
-            byggesak.kategori = new ProsesskategoriType() { kode = "RS", beskrivelse = "Søknad om rammetillatelse" };
-            byggesak.tiltakstype = new[] { new TiltaktypeType() { kode = "nyttbyggboligformal", beskrivelse = "Nytt bygg - boligformål" } };
-            byggesak.vedtak = new VedtakType() { beskrivelse = "Vedtak om rammetillatelse", status = new VedtakstypeType() { kode = "1", beskrivelse = "Godkjent" }, vedtaksdato = DateTime.Now };
+            var byggesak = new GenerateN1().GenerateSample();
 
-            byggesak.matrikkelopplysninger = new MatrikkelopplysningerType();
-            byggesak.matrikkelopplysninger.adresse = new[] {
-                new AdresseType(){ adressekode = "1001", adressenavn = "Byggestedgate", adressenummer = "1" } };
-            byggesak.matrikkelopplysninger.eiendomsidentifikasjon = new[] {
-                new MatrikkelnummerType() { kommunenummer = "9999", gaardsnummer = "1", bruksnummer = "2" } };
-            byggesak.matrikkelopplysninger.bygning = new[] { new BygningType() { bebygdAreal = 110.5, bebygdArealSpecified = true, bygningstype = new BygningstypeType() { kode = "111", beskrivelse = "Enebolig" } } };
-
-            //Osv
             var serializer = new System.Xml.Serialization.XmlSerializer(byggesak.GetType());
             var stringWriter = new Utf8StringWriter();
             serializer.Serialize(stringWriter, byggesak);
             string xml = stringWriter.ToString();
 
 
-       
-
-
+     
             //*** Vedleggstyper
             // Situasjonsplan, Avkjoerselsplan, 
             // TegningEksisterendePlan, TegningNyPlan, TegningEksisterendeSnitt, TegningNyttSnitt, TegningEksisterendeFasade, TegningNyFasade
