5f198cd98ba4c8ec22a495ca510b7d0f3e2afcf9
Tor Kjetil Nilsen <tor@arkitektum.no>
2018-05-08 09:06:19 +0200

    add svarut

--- Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/Program.cs
+++ Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/Program.cs
@@ -1,12 +1,17 @@
 ﻿using no.geointegrasjon.rep.matrikkelfoering;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Geointegrasjon.Matrikkelfoering.Sample
 {
+    public sealed class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding { get { return Encoding.UTF8; } }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -26,8 +31,12 @@ namespace Geointegrasjon.Matrikkelfoering.Sample
             byggesak.matrikkelopplysninger.eiendomsidentifikasjon = new[] {
                 new MatrikkelnummerType() { kommunenummer = "9999", gaardsnummer = "1", bruksnummer = "2" } };
             byggesak.matrikkelopplysninger.bygning = new[] { new BygningType() { bebygdAreal = 110.5, bebygdArealSpecified = true, bygningstype = new BygningstypeType() { kode = "111", beskrivelse = "Enebolig" } } };
-            
+
             //Osv
+            var serializer = new System.Xml.Serialization.XmlSerializer(byggesak.GetType());
+            var stringWriter = new Utf8StringWriter();
+            serializer.Serialize(stringWriter, byggesak);
+            string xml = stringWriter.ToString();
 
 
             //TODO Prossesskategori
@@ -36,7 +45,8 @@ namespace Geointegrasjon.Matrikkelfoering.Sample
             //Vedleggstyper - Situasjonsplan, 
 
             //Opplasting FIKS
-
+            var svarut = new SvarUtService();
+            svarut.Send(byggesak, "123456789", "Matrikkelføring klient");
 
         }
     }
---
+++ Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/SvarUtService.cs
@@ -0,0 +1,59 @@
+﻿using no.geointegrasjon.rep.matrikkelfoering;
+using KommIT.FIKS.AdapterAltinnSvarUt.Services.WS.SvarUt.Forsendelse8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geointegrasjon.Matrikkelfoering.Sample
+{
+    public class SvarUtService
+    {
+
+       public void Send(ByggesakType byggesak, string sendToOrganizationNumber, string sendToName)
+        {
+
+            forsendelse forsendelse = new forsendelse
+            {
+                avgivendeSystem = "Saksbehandlingssystem",
+                eksternref = byggesak.systemId,
+
+                metadataFraAvleverendeSystem = new noarkMetadataFraAvleverendeSakssystem
+                {
+                    tittel = byggesak.tittel
+                },
+                metadataForImport = new noarkMetadataForImport
+                {
+                    saksaar = 2018,
+                    sakssekvensnummer = 12345,
+                    tittel = byggesak.tittel,
+                    journalposttype = "I"
+                },
+                forsendelseType = "Geointegrasjon.Matrikkelføring",
+                mottaker = new adresse()
+                {
+                    digitalAdresse = new organisasjonDigitalAdresse()
+                    {
+                        orgnr = sendToOrganizationNumber
+                    },
+                    postAdresse = new postAdresse()
+                    {
+                        navn = sendToName,
+                        postnr = "9999",
+                        poststed = "Digital levering"
+                    }
+
+                },
+                kunDigitalLevering = true,
+                tittel = byggesak.tittel,
+                //dokumenter = 
+            };
+
+            //TODO send forsendelse to svarut
+
+        }
+
+
+    }
+}
