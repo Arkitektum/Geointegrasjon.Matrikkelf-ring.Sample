ce29c452203ecacc4f6726ab55510f5c0a5766a8
Robert Nordan <robert.nordan@norkart.no>
2018-05-12 00:21:22 +0200

    All of the svarinn actions implemented
    Still need to decrypt downloaded message and send response.

--- Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.ReceiveSample/Program.cs
+++ Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.ReceiveSample/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -11,43 +13,118 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
 {
     class Program
     {
+        /// <summary>
+        /// Forsendelsetypen som mottakstjenester i FIKS filterer meldinger på for å hente matrikkelføringsbeskjeder
+        /// fra eByggeSak til matrikkelklienter.
+        /// </summary>
+        private const string ForsendelsesTypeGeointegrasjonMatrikkel = "Geointegrasjon.Matrikkelføring";
+
+        /// <summary>
+        /// Forsendelsetypen som mottakstjenester i FIKS filterer meldinger på for å hente responsen fra matrikkelsystemet
+        /// som skal tilbake til eBYggesak-systemet.
+        /// </summary>
+        private const string ForsendelsesTypeGeointegrasjonMatrikkelRespons = "Geointegrasjon.Matrikkelføringsrespons";
+
+        // https://github.com/ks-no/svarut-dokumentasjon/wiki/mottaksservice-REST for dokumentasjon av APIet for SvarInn
+        /// <summary>
+        /// URLen man GETer fra i SvarUt for å hente ventende forsendelser.
+        /// </summary>
+        private const string UrlHentNyeForsendelser = "https://test.svarut.ks.no/tjenester/svarinn/mottaker/hentNyeForsendelser";
+
+        /// <summary>
+        /// URLen man POSTer til i SvarUt for å si at man har tatt imot forsendelsen
+        /// </summary>
+        private const string UrlMottakVelykket = "https://svarut.ks.no/tjenester/svarinn/kvitterMottak/forsendelse/{0}"; //+ forsendelsesid
+
+        /// <summary>
+        /// URLen man POSTer til i SvarUt for å si at man ikke kan gjøre noe nyttig med forsendelsen.
+        /// </summary>
+        private const string UrlMottakFeilet = "https://svarut.ks.no/tjenester/svarinn/mottakFeilet/forsendelse/{0}"; //+ forsendelsesid
+
+        /// <summary>
+        /// JSON man POSTer for å si ifra om at man ikke kan håndtere en melding som er kommet inn fordi den ikke er Geointegrasjon.Matrikkelføring
+        /// </summary>
+        private const string ErrorReceiptMessage = "{ \"feilmelding\":\"Kan ikke håndtere forsendelsestypen.\", \"permanent\":true}";
+
         static void Main()
         {
-            Task t = new Task(RunProgram);
-            t.Start();
-            Console.ReadLine();
+            RunProgram().Wait();
         }
 
-        static async void RunProgram()
+        static async Task RunProgram()
         { 
             HttpClient client = CreateClient();
             //Fetch list of waiting messages
-            var fetchWaitingMessagesRequest = new HttpRequestMessage(HttpMethod.Get, "/mottaker/hentNyeForsendelser");
-            AuthenticationHeaderValue authHeader = createAuthHeader();
-            fetchWaitingMessagesRequest.Headers.Authorization = authHeader;
-            HttpResponseMessage response = await client.SendAsync(fetchWaitingMessagesRequest);
+            HttpResponseMessage response = await client.GetAsync(UrlHentNyeForsendelser);
 
             if (!response.IsSuccessStatusCode)
             {
                 throw new Exception("Kunne ikke hente nye forsendelser, sjekk brukernavn og passord.");
             }
+
+            //Download and parse incoming message
+            string responseText = await response.Content.ReadAsStringAsync();
+
+            JArray responseList = JArray.Parse(responseText);
+
             Console.WriteLine("Hentet liste over ventende meldinger:");
             //For each waiting message:
-            Console.WriteLine(response.ToString());
-            //Check if it's the right type (making sure FIKS is not misconfigured)
-            //If not, deny handling the message
+            foreach (JObject waitingMessage in responseList)
+            {
+                string tittel = (string)waitingMessage["tittel"];
+                string id = (string)waitingMessage["id"];
+                string downloadUrl = (string)waitingMessage["downloadUrl"];
 
-            //If OK, download files (Always a zip in our example, can be PDF)
-            //Send receipt that message was handled
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Tittel: " + tittel);
+                Console.WriteLine("Forsendelses-ID: " + id);
+
+                //Check if it's the right type (making sure FIKS is not misconfigured)                
+                if ((string)waitingMessage["forsendelseType"] != ForsendelsesTypeGeointegrasjonMatrikkel)
+                {
+                    //If not, deny handling the message by sending a negative receipt
+                    Console.WriteLine("Kan ikke håndtere denne meldingen, sender  negativ håndteringsrespons");
+                    await client.PostAsync(string.Format(UrlMottakFeilet, id), new StringContent(ErrorReceiptMessage));
+
+                    continue;
+                }
+
+                Console.WriteLine("Melding er av riktig forsendelsestype.");
+
+                //If OK, download files (Always a zip in our example, can be PDF)
+                using (FileStream stream = new FileStream("forsendelse_" + id, FileMode.Create))
+                {
+                    HttpResponseMessage fileResponse = await client.GetAsync(downloadUrl);
+                    await fileResponse.Content.CopyToAsync(stream);
+                }
+
+                //TODO: Decrypt with certificate (also add to instructions about generating and uploading cert)
+
+                Console.WriteLine("Melding er lastet ned.");
+
+                //Send receipt that message was handled
+                //TODO: commented out right now for the sake of testing without having to push new meassages
+                //await client.PostAsync(string.Format(UrlMottakVelykket, id), new StringContent(""));
+
+                //Pretend we do something interesting with the data, then press any key
+                Console.WriteLine("Trykk enter for å sende beskjed om at matrikkelen har (liksom) blitt oppdatert.");
+                Console.ReadLine();
+
+                //Send a return message that the data has been acted on
+                //TODO: Implement stuff using SvarUtService here!
+
+
+                Console.WriteLine("Trykk enter for å gå videre til neste melding.");
+                Console.ReadLine();
+
+            }
         }
 
         private static HttpClient CreateClient()
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://test.svarut.ks.no/tjenester/svarinn");
-
-            //AuthenticationHeaderValue authHeader = createAuthHeader();
-            //client.DefaultRequestHeaders.Authorization = authHeader;
+            AuthenticationHeaderValue authHeader = createAuthHeader();
+            client.DefaultRequestHeaders.Authorization = authHeader;
             return client;
         }
 
