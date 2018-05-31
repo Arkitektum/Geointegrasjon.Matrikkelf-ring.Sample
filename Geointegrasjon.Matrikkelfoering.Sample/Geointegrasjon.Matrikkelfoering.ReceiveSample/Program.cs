c61806b76513e9cb7875e617cd134371c4c97eae
Robert Nordan <robert.nordan@norkart.no>
2018-05-31 15:33:00 +0200

    Add matrikkelføringsresponse

--- Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.ReceiveSample/Program.cs
+++ Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.ReceiveSample/Program.cs
@@ -1,17 +1,19 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography.Pkcs;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
+using Geointegrasjon.Matrikkelfoering.SendSample;
+using no.geointegrasjon.rep.matrikkelfoeringsrespons;
+using no.geointegrasjon.rep.matrikkelfoering;
+using KommIT.FIKS.AdapterAltinnSvarUt.Services.WS.SvarUt.Forsendelse8;
 
 namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
 {
@@ -44,10 +46,11 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
         /// URLen man POSTer til i SvarUt for å si at man ikke kan gjøre noe nyttig med forsendelsen.
         /// </summary>
         private const string UrlMottakFeilet = "https://test.svarut.ks.no/tjenester/svarinn/mottakFeilet/forsendelse/{0}"; //+ forsendelsesid
-      
+
         static void Main()
         {
             RunProgram().Wait();
+            Console.ReadLine();
         }
 
         static async Task RunProgram()
@@ -63,13 +66,13 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
 
             //Download and parse incoming message
             string responseText = await response.Content.ReadAsStringAsync();
-            JArray responseList = JArray.Parse(responseText);
+            List<Forsendelse> responseList = JsonConvert.DeserializeObject<List<Forsendelse>>(responseText);
 
             Console.WriteLine("Hentet liste over ventende meldinger:");
             if (responseList.Count > 0)
             {
                 //For each waiting message:
-                foreach (JObject waitingMessage in responseList)
+                foreach (Forsendelse waitingMessage in responseList)
                 {
                     await ProcessWaitingMessage(client, waitingMessage);
                 }
@@ -80,20 +83,18 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
             }
         }
 
-        private static async Task ProcessWaitingMessage(HttpClient client, JObject waitingMessage)
+        private static async Task ProcessWaitingMessage(HttpClient client, Forsendelse message)
         {
-            MottattMelding message = new MottattMelding(waitingMessage);
-
             Console.WriteLine("--------------------------------");
-            Console.WriteLine("Tittel: " + message.Tittel);
-            Console.WriteLine("Forsendelses-ID: " + message.Id);
+            Console.WriteLine("Tittel: " + message.tittel);
+            Console.WriteLine("Forsendelses-ID: " + message.id);
 
             //Check if it's the right type (making sure FIKS is not misconfigured)                
-            if (message.Forsendelsestype != ForsendelsesTypeGeointegrasjonMatrikkel)
+            if (message.forsendelseType != ForsendelsesTypeGeointegrasjonMatrikkel)
             {
                 //If not, deny handling the message by sending a negative receipt
                 Console.WriteLine("Kan ikke håndtere denne meldingen, sender  negativ håndteringsrespons");
-                await DenyHandlingMessage(client, message.Id);
+                await DenyHandlingMessage(client, message.id);
                 return;
             }
 
@@ -101,13 +102,13 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
 
             //If OK, download files (Always a zip in our example, can be a single PDF in other cases)
 
-            await DownloadAndDecryptMessageFile(client, message.Id, message.DownloadUrl);
+            await DownloadAndDecryptMessageFile(client, message.id, message.downloadUrl);
+            ByggesakType byggesak = await readByggeSakXml(message);
 
-            Console.WriteLine("Melding er lastet ned og dekryptert.");
+            Console.WriteLine("Melding er lastet ned, dekryptert og innlest.");
 
             //Send receipt that message was handled
-            //TODO: commented out right now for the sake of testing without having to push new messages
-            //await client.PostAsync(string.Format(UrlMottakVelykket, id), new StringContent(""));
+            await client.PostAsync(string.Format(UrlMottakVelykket, message.id), new StringContent(""));
             Console.WriteLine("Mottak av melding bekreftet til SvarInn");
 
             //Pretend we do something interesting with the data, then possibly send a response
@@ -116,13 +117,69 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
 
             if (sendProcessedResponse)
             {
-                //Send a return message that the data has been acted on
-                //TODO: Implement stuff using SvarUtService here! Need to define the message format first!
+                //Send a return message that the data has been acted on                
+                SendMatrikkelFoeringsResponse(message, byggesak);
                 Console.WriteLine("Returbeskjed sendt, går videre til neste melding.");
             }
             else
             {
                 Console.WriteLine("Går videre til neste melding uten å sende returbeskjed.");
+            }
+        }
+
+        private static void SendMatrikkelFoeringsResponse(Forsendelse message, ByggesakType byggesak)
+        {
+            string responseXml = PrepareResponseXML(message, byggesak);
+
+            List<dokument> dokumenter = new List<dokument>();
+            dokument matrikkelRespons = new dokument()
+            {
+                dokumentType = "ByggesakMatrikkelFøringsRespons",
+                data = System.Text.Encoding.UTF8.GetBytes(responseXml),
+                filnavn = "matrikkelrespons.xml",
+                mimetype = "application/xml"
+            };
+            dokumenter.Add(matrikkelRespons);
+            
+            string orgnrTilKommunen = ConfigurationManager.AppSettings["OrgNrReceiver"];
+            SvarUtService.Send("matrikkelsystem", ForsendelsesTypeGeointegrasjonMatrikkelRespons, "Matrikkelføringsrespons for " + message.tittel, "12345", orgnrTilKommunen, "Matrikkelføringsrespons klient", dokumenter.ToArray());
+        }
+
+        private static string PrepareResponseXML(Forsendelse message, ByggesakType byggesak)
+        {
+            string[] bygningsNummer;
+            if (byggesak.matrikkelopplysninger == null)
+            {
+                //We make up something to pretend we've added a new building
+                Random rnd = new Random();
+                bygningsNummer = new string[] { rnd.Next(10000, 100000) .ToString() };
+            }
+            else
+            {
+                bygningsNummer = byggesak.matrikkelopplysninger.bygning.Select(bygning => bygning.bygningsnummer).ToArray();
+            }
+
+            MatrikkelFoeringsResponsType responseMessage = new MatrikkelFoeringsResponsType()
+            {
+                bygningsnummer = bygningsNummer,
+                saksnummer = new no.geointegrasjon.rep.matrikkelfoeringsrespons.SaksnummerType()
+                {
+                    //Tostring because XSD.exe maps xml:integer to string because it's not bounded like int32
+                    //TODO: Check assumption that ids may be larger than int32
+                    saksaar = message.metadataFraAvleverendeSystem.saksaar.ToString(),
+                    sakssekvensnummer = message.metadataFraAvleverendeSystem.sakssekvensnummer.ToString()
+                }
+            };
+            return writeMatrikkelFoeringsResponsXML(responseMessage);
+        }
+
+        private static async Task<ByggesakType> readByggeSakXml(Forsendelse message)
+        {
+            string byggesakXML = await ReadByggesakXMLFromZip(message);
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(ByggesakType));
+            using (var stringReader = new StringReader(byggesakXML))
+            {
+                return (ByggesakType)serializer.Deserialize(stringReader);
             }
         }
 
@@ -145,6 +202,25 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
             File.WriteAllBytes("forsendelse_" + id + ".zip", cmsData.ContentInfo.Content);
         }
 
+        private static async Task<string> ReadByggesakXMLFromZip(Forsendelse message)
+        {
+            //Get the filename from metadata
+
+            //Read the file to extract the XML
+            using (FileStream zipToOpen = new FileStream("forsendelse_" + message.id + ".zip", FileMode.Open))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    //TODO: byggesak.xml from dkument type!!!!
+                    ZipArchiveEntry entry = archive.GetEntry("byggesak.xml");
+                    using (StreamReader reader = new StreamReader(entry.Open()))
+                    {
+                        return await reader.ReadToEndAsync();
+                    }
+                }
+            }
+        } 
+
         private static HttpClient CreateClient()
         {
             var client = new HttpClient();
@@ -161,6 +237,15 @@ namespace Geointegrasjon.Matrikkelfoering.ReceiveSample
             var byteArray = new UTF8Encoding().GetBytes(String.Format("{0}:{1}", mottakUserName, mottakPassword));
             AuthenticationHeaderValue authHeader = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
             return authHeader;
+        }
+
+        private static string writeMatrikkelFoeringsResponsXML(MatrikkelFoeringsResponsType respons)
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(respons.GetType());
+            var stringWriter = new Utf8StringWriter();
+            serializer.Serialize(stringWriter, respons);
+            string xml = stringWriter.ToString();
+            return xml;
         }
 
     }
